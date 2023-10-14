72eac89109dd59eb4184556515882c788fd6b111
marcotedeschini <234345@studenti.unimore.it>
2023-10-14 17:20:17 +0200

    First working attempt of RADIO-DISH version

--- draft_test/client.c
+++ draft_test/client.c
@@ -2,68 +2,38 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-
-// Flag to indicate if a keyboard interruption has been received
-volatile sig_atomic_t interrupted = 0;
-
-/**
- * @brief Handle the keyboard interruption (Ctrl+C)
- * @param signal
- */
-void handle_interrupt(int sig) {
-    (void) sig; // Avoid unused parameter warning
-    interrupted = 1;
-    printf("\n[WARNING]: Keyboard interruption received (Ctrl+C)\n");
-    exit(0);
-}
+#include <unistd.h> // for sleep()
 
 int main(void) {
-    signal(SIGINT, handle_interrupt); // Register the interruption handling function
+    int rc;
 
-    // Create a new ZeroMQ context. This is the first step in working with ZeroMQ and is needed to manage sockets.
     void *ctx = zmq_ctx_new();
-    assert(ctx);  // Ensure the context was successfully created.
+    assert(ctx);
 
-    // Create a new socket of type DGRAM. It's used for sending and receiving UDP datagrams.
-    void *sender = zmq_socket(ctx, ZMQ_DGRAM);
-    assert(sender);  // Ensure the socket was successfully created.
+    void *radio = zmq_socket(ctx, ZMQ_RADIO);
+    assert(radio);
+    assert(zmq_connect(radio, "udp://127.0.0.1:5556") == 0);
 
-    // Bind the socket to the local address and port. The client will listen on port 5557.
-    int rc = zmq_bind(sender, "udp://127.0.0.1:5555");
-    assert(rc == 0);  // Ensure the binding was successful.
+    while (1) {
+        const char *msg = "Hello";
+        zmq_msg_t message;
+        zmq_msg_init_size(&message, strlen(msg));
+        memcpy(zmq_msg_data(&message), msg, strlen(msg));
+        rc = zmq_msg_set_group(&message, "GRP");
+        assert(rc == 0);
 
-    // Prepare the message and the destination address.
-    const char *msg = "Is someone there?";  // The message to send.
-    const char *address = "127.0.0.1:5556";  // The server's address (to send messages to).
+        rc = zmq_msg_send(&message, radio, 0);
+        if (rc == -1) {
+            printf("Error in sending message\n");
+            zmq_msg_close(&message);
+            break;
+        }
 
-    while (!interrupted) {
-        // First, send the address and then the message body to the server.
-        assert(zmq_send(sender, address, strlen(address), ZMQ_SNDMORE) != -1);
-        assert(zmq_send(sender, msg, strlen(msg), 0) != -1);
-
-        // Prepare to receive a reply from the server.
-        char reply_addr[256];  // Buffer for the reply address
-        char reply[256];  // Buffer for the reply body
-        rc = zmq_recv(sender, reply_addr, 255, 0);
-        assert(rc != -1);
-        reply_addr[rc] = '\0';  // Ensure it's null-terminated
-        rc = zmq_recv(sender, reply, 255, 0);
-        assert(rc != -1);
-        reply[rc] = '\0';  // Ensure it's null-terminated
-
-        // Print the response received from the server.
-        printf("Received: %s\n", reply);
-
-        // Sleep for 2 seconds
-        sleep(2);
+        zmq_msg_close(&message);
+        sleep(1); // Send a message every second
     }
 
-    // Close the socket and destroy the ZeroMQ context. These are important steps to ensure all resources are properly cleaned up.
-    zmq_close(sender);
+    zmq_close(radio);
     zmq_ctx_destroy(ctx);
-
-    return 0;  // End the program.
+    return 0;
 }
--- draft_test/server.c
+++ draft_test/server.c
@@ -2,60 +2,34 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-
-// Flag to indicate if a keyboard interruption has been received
-volatile sig_atomic_t interrupted = 0;
-
-/**
- * @brief Handle the keyboard interruption (Ctrl+C)
- * @param signal
- */
-void handle_interrupt(int sig) {
-    (void) sig; // Avoid unused parameter warning
-    interrupted = 1;
-    printf("\n[WARNING]: Keyboard interruption received (Ctrl+C)\n"); // Translated into English
-    exit(0);
-}
+#include <unistd.h> // for sleep()
 
 int main(void) {
-    signal(SIGINT, handle_interrupt); // Register the interruption handling function
-    // Create a new ZeroMQ context. This is necessary for almost all ZeroMQ operations.
+
+    int rc;
     void *ctx = zmq_ctx_new();
-    assert(ctx);  // Ensure the context is not NULL (i.e., it has been successfully created).
+    assert(ctx);
 
-    // Create a new ZeroMQ socket of type DGRAM. This socket type is used for sending and receiving UDP datagrams.
-    void *receiver = zmq_socket(ctx, ZMQ_DGRAM);
-    assert(receiver);  // Verify that the socket creation was successful.
+    void *dish = zmq_socket(ctx, ZMQ_DISH);
+    assert(dish);
+    assert(zmq_bind(dish, "udp://127.0.0.1:5556") == 0);
 
-    // Bind the socket to the specified local address and port. In this case, it will listen on all network interfaces at port 5556.
-    assert(zmq_bind(receiver, "udp://127.0.0.1:5556") == 0);  // Verify that the binding was successful.
+    rc = zmq_join(dish, "GRP");
+    assert(rc == 0);
 
-    // Infinite loop to receive messages.
-    while (!interrupted) {
-        char address[256];  // Buffer for the address of the incoming message
-        char message[256];  // Buffer for the body of the incoming message
+    while (1) {
+        char buffer[1024];
+        rc = zmq_recv(dish, buffer, 1023, 0);
+        if (rc == -1) {
+            printf("Error in receiving message\n");
+            break;
+        }
 
-        // Receive the address and then the message body.
-        assert(zmq_recv(receiver, address, 255, 0) != -1);
-
-        assert(zmq_recv(receiver, message, 255, 0) != -1);
-
-        // Print the received message.
-        printf("Received: %s\n", message);
-
-        const char *response = "Yes, there is!";  // The response that will be sent back.
-
-        // Send the address first and then the response message.
-        assert(zmq_send(receiver, address, strlen(address), ZMQ_SNDMORE) != -1);
-        assert(zmq_send(receiver, response, strlen(response), 0) != -1);
+        buffer[rc] = '\0'; // Null-terminate the string
+        printf("Received: %s\n", buffer);
     }
 
-    // Close the socket and destroy the ZeroMQ context. In a real application, these calls are important for proper resource management.
-    // Note: These lines are not reached in this code due to the infinite while(1) loop. You should handle an exit signal or another termination condition to properly exit the loop and perform cleanup.
-    zmq_close(receiver);
+    zmq_close(dish);
     zmq_ctx_destroy(ctx);
-
     return 0;
 }
